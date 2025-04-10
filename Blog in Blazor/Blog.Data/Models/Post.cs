ab9827d81a683fa548eb2d44bbb9051596e401d5
Istvan.HL6SHP <hl6shp@inf.elte.hu>
2025-04-10 19:55:22 +0200

    Implemented Task 2

--- Blog in Blazor/Blog.Data/Models/Post.cs
+++ Blog in Blazor/Blog.Data/Models/Post.cs
@@ -15,6 +15,8 @@ namespace Blog.Data.Models
 
         public DateTime CreatedDate { get; set; }
 
+        public DateTime UpdatedDate { get; set; }
+
         public string CreatedBy { get; set; }
 
         public string? UpdatedBy { get; set; }
